5dd52fb31298ed92834976c6d6d19393cadd3dde
Yan Avlis <ccyan05@alu.ufc.br>
2024-05-15 14:18:30 -0300

    Main

--- src/main/java/com/app/Main.java
+++ src/main/java/com/app/Main.java
@@ -1,7 +1,24 @@
 package main.java.com.app;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Integer[] table = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int iterativeSum = 0;
+
+        for (int i = 0; i < table.length; i++) {
+            iterativeSum += table[i];
+        }
+
+        List<Integer> list = Arrays.asList(table);
+
+        int functionalSum = list
+                .stream()
+                .reduce(0, (a, b) -> a + b);
+
+        System.out.println(iterativeSum);
+        System.out.println(functionalSum);
     }
 }
