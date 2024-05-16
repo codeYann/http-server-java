255b5bb041e4a5b82cccd2a827a35d56b7963abc
Yan Avlis <ccyan05@alu.ufc.br>
2024-05-16 15:20:23 -0300

    Streams & Collections

--- src/main/java/com/app/Main.java
+++ src/main/java/com/app/Main.java
@@ -1,24 +1,51 @@
 package main.java.com.app;
 
 import java.util.*;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+class User {
+    public String name;
+    public int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "User: " + this.name;
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
+        List<User> users = new LinkedList<>() {
+            {
+                add(new User("Bob", 40));
+                add(new User("Jane", 30));
+                add(new User("John", 20));
+                add(new User("Martin", 12));
+                add(new User("Phillip", 56));
+                add(new User("Hilbert", 59));
+            }
+        };
 
-        Integer[] table = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        int iterativeSum = 0;
-
-        for (int i = 0; i < table.length; i++) {
-            iterativeSum += table[i];
-        }
-
-        List<Integer> list = Arrays.asList(table);
-
-        int functionalSum = list
+        var v = users
                 .stream()
-                .reduce(0, (a, b) -> a + b);
+                .filter(u -> u.getAge() > 50)
+                .collect(Collectors.toUnmodifiableList());
 
-        System.out.println(iterativeSum);
-        System.out.println(functionalSum);
+        v.forEach(System.out::println);
+
     }
 }
