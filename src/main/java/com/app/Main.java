c8b44c6da602afb023e69875492ccf8d974ee342
Yan Avlis <ccyan05@alu.ufc.br>
2024-05-17 10:20:16 -0300

    Group by stream

--- src/main/java/com/app/Main.java
+++ src/main/java/com/app/Main.java
@@ -1,16 +1,26 @@
 package main.java.com.app;
 
+import java.rmi.server.ExportException;
 import java.util.*;
-import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
+enum Role {
+    MODERATOR,
+    DEVELOPER,
+    NONE,
+}
+
 class User {
+    int id;
     public String name;
     public int age;
+    public Role role;
 
-    public User(String name, int age) {
+    public User(int id, String name, int age, Role role) {
+        this.id = id;
         this.name = name;
         this.age = age;
+        this.role = role;
     }
 
     public int getAge() {
@@ -20,6 +30,10 @@ class User {
     public String getName() {
         return name;
     }
+
+    public Role getRole() { return role; }
+
+    public int getId() { return id; }
 
     @Override
     public String toString() {
@@ -31,21 +45,23 @@ public class Main {
     public static void main(String[] args) {
         List<User> users = new LinkedList<>() {
             {
-                add(new User("Bob", 40));
-                add(new User("Jane", 30));
-                add(new User("John", 20));
-                add(new User("Martin", 12));
-                add(new User("Phillip", 56));
-                add(new User("Hilbert", 59));
+                add(new User(1, "Bob", 40, Role.NONE));
+                add(new User(2, "Jane", 30, Role.NONE));
+                add(new User(3,"John", 20, Role.DEVELOPER));
+                add(new User(4,"Martin", 12, Role.DEVELOPER));
+                add(new User(5,"Phillip", 56, Role.MODERATOR));
+                add(new User(6, "Hilbert", 59, Role.MODERATOR));
             }
         };
 
-        var v = users
+        Map<Integer, List<User>> map = users
                 .stream()
-                .filter(u -> u.getAge() > 50)
-                .collect(Collectors.toUnmodifiableList());
+                .filter(user -> user.role == Role.MODERATOR)
+                .collect(Collectors.groupingBy(User::getId));
 
-        v.forEach(System.out::println);
-
+        map.forEach((role, list) -> {
+            System.out.println(role);
+            System.out.println(list);
+        });
     }
 }
